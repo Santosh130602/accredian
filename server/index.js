c99299efdfd4d54e52f82bfe912ccb1d41db3cd0
Santosh Kumar <159407236+Santosh130602@users.noreply.github.com>
2024-07-06 23:46:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,13 @@ const referralRoutes = require('./routes/referralRoutes');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: JSON.parse(process.env.CORS_ORIGIN),
+    credentials: true,
+    maxAge: 14400,
+  })
+);
 app.use(bodyParser.json());
 
 // Connect to MongoDB
